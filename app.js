b71476b7926bfc08eeac4112a9942c00263e9a53
krishna <kkrajus777@gmail.com>
2019-07-19 10:23:12 -0500

    feat(app): missing update and delete methods

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const stuff = require('../fixtures/stuff')
+const stuff = require('./fixtures/stuff')
 const Thing = require('./models/thing')
 
 const express = require('express')
@@ -48,6 +48,56 @@ app.get('/api/stuff', (req, res, next)=>{
             error:error
         })
     })
-})
+}).get('/api/stuff/:id', (req, res, next) => {
+    Thing.findOne({
+        _id: req.params.id
+    }).then(
+        (thing) => {
+            res.status(200).json(thing);
+        }
+    ).catch(
+        (error) => {
+            res.status(404).json({
+                error: error
+            });
+        }
+    );
+}).put('/api/stuff/:id', (req, res, next) => {
+    const thing = new Thing({
+        _id: req.params.id,
+        title: req.body.title,
+        description: req.body.description,
+        imageUrl: req.body.imageUrl,
+        price: req.body.price,
+        userId: req.body.userId
+    });
+    Thing.updateOne({_id: req.params.id}, thing).then(
+        () => {
+            res.status(201).json({
+                message: 'Thing updated successfully!'
+            });
+        }
+    ).catch(
+        (error) => {
+            res.status(400).json({
+                error: error
+            });
+        }
+    );
+}).delete('/api/stuff/:id', (req, res, next) => {
+    Thing.deleteOne({_id: req.params.id}).then(
+        () => {
+            res.status(200).json({
+                message: 'Deleted!'
+            });
+        }
+    ).catch(
+        (error) => {
+            res.status(400).json({
+                error: error
+            });
+        }
+    );
+});
 
 module.exports = app
---
+++ fixtures/stuff.js
@@ -0,0 +1,20 @@
+module.exports = ()=>{
+    return [
+        {
+            _id: 'oeihfzeoi',
+            title: 'My first thing',
+            description: 'All of the info about my first thing',
+            imageUrl: 'https://bit.ly/2SgYrOS',
+            price: 4900,
+            userId: 'qsomihvqios',
+        },
+        {
+            _id: 'oeihfzeomoihi',
+            title: 'My second thing',
+            description: 'All of the info about my second thing',
+            imageUrl: 'https://bit.ly/2SgYrOS',
+            price: 2900,
+            userId: 'qsomihvqios',
+        },
+    ];
+}
